159867ced3ce5337d4033f34f8b3b9bd7abc161b
Shmully Wolfson <shmullywolfson@gmail.com>
2016-11-22 12:50:15 -0500

    create lists table

--- db/migrate/20161122171735_create_list.rb
+++ db/migrate/20161122171735_create_list.rb
@@ -1,4 +1,9 @@
 class CreateList < ActiveRecord::Migration
   def change
+    creat_table :lists do |t|
+      t.string :title
+      t.boolean :complete, default: false
+      t.timestamps null: false
+    end
   end
 end
