76d07a8699ff6e3eb141432e7aeeba209b59783c
Shmully Wolfson <shmullywolfson@gmail.com>
2016-11-22 12:55:02 -0500

    create list_items table

--- db/migrate/20161122171934_create_list_items.rb
+++ db/migrate/20161122171934_create_list_items.rb
@@ -1,4 +1,10 @@
 class CreateListItems < ActiveRecord::Migration
   def change
+    creat_table :list_items do |t|
+      t.string :name
+      t.integer :list_id
+      t.boolean :complete, default: false
+      t.timestamps null: false
+    end
   end
 end
