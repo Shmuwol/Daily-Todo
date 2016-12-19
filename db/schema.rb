4082fd0f2b6daebf35cfd810f66e22c8179c4398
Shmully Wolfson <shmullywolfson@gmail.com>
2016-12-19 16:55:25 -0500

    add schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216162403) do
+ActiveRecord::Schema.define(version: 20161219212958) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
     t.boolean  "complete",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -26,6 +27,14 @@ ActiveRecord::Schema.define(version: 20161216162403) do
     t.boolean  "complete",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
