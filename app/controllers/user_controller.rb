8067b6a2ec4b8d787a7002310b6efdad3b742f97
Shmully Wolfson <shmullywolfson@gmail.com>
2016-12-26 15:48:38 -0500

    add post users route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,13 @@ class UsersController < ApplicationController
   get '/sign_up' do
     erb :'/users/new'
   end
+
+  post '/users' do
+    user = User.create(params)
+    if user.save
+      redirect to '/lists'
+    else
+      redirect to '/sign_up'
+    end
+  end
 end
