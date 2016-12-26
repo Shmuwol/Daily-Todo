072002b4261cfdce5afc957928cbb4823fe67bc1
Shmully Wolfson <shmullywolfson@gmail.com>
2016-12-26 13:51:43 -0500

    add sign_up route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,30 +1,6 @@
 class UsersController < ApplicationController
 
-  get "/users" do
-    erb :"/users/index"
-  end
-
-  get "/users/new" do
-    erb :"/users/new"
-  end
-
-  post "/users" do
-    redirect "/users"
-  end
-
-  get "/users/:id" do
-    erb :"/users/show"
-  end
-
-  get "/users/:id/edit" do
-    erb :"/users/edit"
-  end
-
-  patch "/users" do
-    redirect "/users/:id"
-  end
-
-  delete "/users/:id/delete" do
-    redirect "/users"
+  get '/sign_up' do
+    erb :'/users/new'
   end
 end
