5c2b2336aac4a20c387248d9dc1e37d294af6248
Shmully Wolfson <shmullywolfson@gmail.com>
2017-01-02 13:25:03 -0500

    add 'slug'ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :lists
   has_secure_password
   validates_presence_of :username, :email
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|user| user.slug == slug}
+  end
 end
