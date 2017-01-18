842001ff15805e90828e98a8e9ad501cbef8cd72
Shmully Wolfson <shmullywolfson@gmail.com>
2017-01-18 13:38:25 -0500

    add /lists/:id/tasks route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
 class TasksController < ApplicationController
+
+  get '/lists/:id/tasks' do
+    redirect_if_not_logged_in
+    @list = current_user.lists.find(params[:id])
+    @tasks = @list.tasks
+    erb :'tasks/index'
+  end
+  
 end
