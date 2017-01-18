cdcaa9fd5627baa93017a8e9adc3826d6bdc4e14
Shmully Wolfson <shmullywolfson@gmail.com>
2017-01-18 13:41:07 -0500

    add /lists/:id/tasks post route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,17 @@ class TasksController < ApplicationController
     @tasks = @list.tasks
     erb :'tasks/index'
   end
-  
+
+  post '/lists/:id/tasks' do
+    redirect_if_not_logged_in
+    @list = current_user.lists.find(params[:id])
+    task = @list.tasks.build(name: params[:name])
+    if task.save
+      redirect to "/lists/#{@list.id}"
+    else
+      flash[:notice] = "Something went wrong, task wasn't saved!"
+      redirect to '/lists'
+    end
+  end
+
 end
