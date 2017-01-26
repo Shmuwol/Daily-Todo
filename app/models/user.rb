5e48c09aae8903b7778ec9e47ccab057de911880
Shmully Wolfson <shmullywolfson@gmail.com>
2017-01-26 13:50:05 -0500

    remove unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :email
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
-  def slug
-    username.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find{|user| user.slug == slug}
-  end
 end
