63b1d8e1eb4b9cffdaa965ac3f6477d9db117418
Shmully Wolfson <shmullywolfson@gmail.com>
2017-01-26 20:55:33 -0500

    add changes from  assessment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :tasks, through: :lists
 
   has_secure_password
-  validates_presence_of :username, :email
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
